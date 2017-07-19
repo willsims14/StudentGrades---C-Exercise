52b7fed113d1bb16ac06dd227d767633bc3f9392
William A Sims III <willsims14@gmail.com>
2017-07-19 10:47:13 -0500

    Add project files.

---
+++ ReadOnlyVariables/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReadOnlyVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
